0606dff86afef730f6ab9e4f06a1a0203d6de89d
AngelCertificatedProfessional <angel.espinozatorres213@gmail.com>
2021-05-07 19:27:42 -0700

    Se eliminaron las sentencias \n ya que no son necesarias para el ncurses

--- menu1.c
+++ menu1.c
@@ -18,11 +18,11 @@ int main() {
 			//En esta seccion se imprime el titulo
 			imprimirTitulo();
 			attron(COLOR_PAIR(1));
-			mvprintw(16,22,"Bienvenid@ al menu de opciones del examen de Introduccion a la Programacion de la UABC\n");
-			mvprintw(17,22,"1.Examen\n");
-			mvprintw(18,22,"2.Consulta\n");
-			mvprintw(19,22,"3.Bajas\n"); 
-			mvprintw(20,22,"4.Salida\n");
+			mvprintw(16,22,"Bienvenid@ al menu de opciones del examen de Introduccion a la Programacion de la UABC");
+			mvprintw(17,22,"1.Examen");
+			mvprintw(18,22,"2.Consulta");
+			mvprintw(19,22,"3.Bajas"); 
+			mvprintw(20,22,"4.Salida");
 			attroff(COLOR_PAIR(1));
 			attron(COLOR_PAIR(2));
 			mvprintw(21,22,"A que desea acceder usted:");
@@ -40,7 +40,7 @@ int main() {
 					break;
 				case 4:
 					erase();
-					mvprintw(1,1,"Que le vaya bien\n");
+					mvprintw(1,1,"Que le vaya bien");
 					refresh();
 					getch();
 					break;
@@ -48,7 +48,7 @@ int main() {
 					erase();
 					bkgd(COLOR_PAIR(7));
 					attron(COLOR_PAIR(7));
-					mvprintw(23,22,"Error 411, La opcion que a introducido es incorrecta\n");
+					mvprintw(23,22,"Error 411, La opcion que a introducido es incorrecta");
 					attroff(COLOR_PAIR(7));
 					refresh();
 					getch();
